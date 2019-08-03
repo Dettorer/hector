db440ffec95e764944d29f1e12baa422ef82061b
Paul Dettorer Hervot <dettorer@dettorer.net>
2019-08-03 16:55:00 +0200

    core: rename play and abort to load and unload

--- commands/load.js
+++ commands/load.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: "play",
-    description: "lance une partie du jeu demandé",
+    name: "load",
+    description: "charge le jeu demandé",
     usage: "<jeu>",
     minArgs: 1,
     help: "",
     execute(message, args, client) {
         if (client.game) {
-            return message.reply(`Une partie de ${client.game.name} est déjà en cours, je ne peux pas lancer autre chose.`)
+            return message.reply(`Un jeu (${client.game.name}) est déjà chargé, il faut d'abord le quitter.`)
         }
 
         if (!client.available_games.has(args[0])) {
-            message.reply(`Je ne connais pas le jeux "${args[0]}"`)
+            message.reply(`Je ne connais pas le jeu "${args[0]}"`)
         } else {
             client.loadGame(args[0], message);
             client.game.play(client, message);
--- commands/unload.js
+++ commands/unload.js
@@ -1,12 +1,12 @@
 module.exports = {
-    name: "abort",
-    description: "annule et termine la partie en cours",
+    name: "unload",
+    description: "décharge le jeu en cours",
     usage: "",
     minArgs: 0,
     help: "",
     execute(message, args, client) {
         if (!client.game) {
-            return message.reply("il n'y a aucun jeu à terminer")
+            return message.reply("aucun jeu n'est actuellement chargé")
         }
 
         client.unloadGame(message);
