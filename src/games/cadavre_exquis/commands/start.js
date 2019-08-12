911798c747a013570769f2572db902b996869762
Paul Dettorer Hervot <dettorer@dettorer.net>
2019-08-12 14:17:42 +0200

    cadavre: fix double start bug

--- src/games/cadavre_exquis/commands/start.js
+++ src/games/cadavre_exquis/commands/start.js
@@ -16,8 +16,15 @@ export const help = "";
  * @param {Array<String>} args - the arguments the user gave to the command
  */
 export function execute(message, args, client) {
+    if (client.game.playing) {
+        return message.reply(`Une partie est déjà en cours, si vous voulez rejoindre les suivantes, faites \`${client.config.prefix}play\`.`);
+    }
+    if (!client.game.pendingPlayers.has(message.author.id)) {
+        return message.reply(`Vous n'avez pas rejoins la partie, vous ne pouvez pas la lancer. Pour rejoindre les parties suivantes, faites \`${client.config.prefix}play\`.`);
+    }
     if (client.game.pendingPlayers.size < 3) {
         return message.reply("Désolé, il faut au moins trois personnes pour jouer.");
     }
+
     client.game.start(client, message);
 }
